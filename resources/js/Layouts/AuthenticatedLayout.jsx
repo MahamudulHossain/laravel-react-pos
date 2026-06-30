27e75b94a803252db754f96efa42ad6af0a0c777
MahamudulHossain <md.mahamudulhossainn@gmail.com>
2026-07-01 01:51:46 +0600

    layout added in pos view

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -49,8 +49,8 @@ export default function AuthenticatedLayout({ header, children }) {
                             </div>
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                 <NavLink
-                                    href={route('pos')}
-                                    active={route().current('pos')}
+                                    href={route('pos.index')}
+                                    active={route().current('pos.index')}
                                 >
                                     POS
                                 </NavLink>
